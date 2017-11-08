ffd37ae2e8870dbe66137e754166cbfa8f8fa70f
julio1204 <33357302+julio1204@users.noreply.github.com>
2017-11-08 09:16:46 -0600

    Clases producto.
    se hace commmit de clases de producto.

---
+++ src/Modelo/Conexionproducto.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Julito
+ */
+public class Conexionproducto {
+    private final String base = "SistemaDeInventarios";
+    private final String user = "root";
+    private final String password = "";
+    private final String url = "jdbc:mysql://localhost:3306/"+base;
+    private Connection con = null;
+    
+    public Connection getConexion()
+    {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+    con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+        }
+        catch(SQLException e)
+                {
+            System.err.println(e);
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexionproducto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+}
